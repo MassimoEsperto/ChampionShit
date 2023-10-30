9961c51122efa27a205c268cfd7372631a918f70
MassimoEsperto <em.decarlo@gmail.com>
2023-10-30 14:35:49 +0100

    fix match live

--- src/app/views/page/dashboard/partita-incorso/partita-incorso.component.ts
+++ src/app/views/page/dashboard/partita-incorso/partita-incorso.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { GlobalComponent } from 'src/app/classes/utils/global-component';
+import { FantaGazzettaService } from 'src/app/services/fanta-gazzetta.service';
 
 @Component({
   selector: 'partita-incorso',
@@ -10,13 +11,30 @@ import { GlobalComponent } from 'src/app/classes/utils/global-component';
 export class PartitaIncorsoComponent extends GlobalComponent implements OnInit {
 
   @Input() dash: any;
+  votilive: any
 
-  
-  constructor( private router: Router) {
+
+  constructor(
+    private router: Router,
+    private fantaService: FantaGazzettaService) {
     super();
   }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.getLivefanta()
+  }
+
+  getLivefanta() {
+
+    this.fantaService.getLiveFormazione()
+
+      .subscribe({
+
+        next: (result: any) => {
+          this.votilive = result;
+        }
+      })
+
   }
 
   visualizzaLive() {
